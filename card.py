8d513e15d99c56e14995fab591978d3770ae78c5
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2021-12-23 14:32:32 +0000

    'tdad

---
+++ card.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n + 1) #price 
+
+p = [0] + list(map(int, input().split())) #입력되는 price
+
+dp[1] = p[1]
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i - j] + p[j])
+
+print(dp[n])
